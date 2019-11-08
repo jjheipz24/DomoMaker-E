49a72b3070e4db5378c988154b5bba11bbb1539e
Jin Jin Heipler <heipler.jj@gmail.com>
2019-11-07 23:18:01 -0500

    Added lives attribute to domo. updated css

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -19,15 +19,16 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req, res) => {
-  if (!req.body.name || !req.body.age) {
+  if (!req.body.name || !req.body.age || !req.body.lives) {
     return res.status(400).json({
-      error: 'RAWR! Both name and age are required',
+      error: 'RAWR! Name, age, and lives are required',
     });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
+    lives: req.body.lives,
     owner: req.session.account._id,
   };
 
--- server/models/Domo.js
+++ server/models/Domo.js
@@ -22,6 +22,13 @@ const DomoSchema = new mongoose.Schema({
     required: true,
   },
 
+  lives: {
+    type: Number,
+    min: 1,
+    required: true,
+
+  },
+
   owner: {
     type: mongoose.Schema.ObjectId,
     required: true,
@@ -37,6 +44,7 @@ const DomoSchema = new mongoose.Schema({
 DomoSchema.statics.toAPI = (doc) => ({
   name: doc.name,
   age: doc.age,
+  lives: doc.lives,
 });
 
 DomoSchema.statics.findByOwner = (ownerId, callback) => {
@@ -44,7 +52,7 @@ DomoSchema.statics.findByOwner = (ownerId, callback) => {
     owner: convertId(ownerId),
   };
 
-  return DomoModel.find(search).select('name age').exec(callback);
+  return DomoModel.find(search).select('name age lives').exec(callback);
 };
 
 DomoModel = mongoose.model('Domo', DomoSchema);
